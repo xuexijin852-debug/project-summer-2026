5b1c48ca8a212a789225b326638b9569ca78ac67
xuexijin852-debug <xuexijin852@gmail.com>
2026-07-22 18:55:19 +0900

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB5ZwOyYeqsPQR3wqTWNaHUGagp2NjHA04",
+  authDomain: "project-summer-2026-12de8.firebaseapp.com",
+  projectId: "project-summer-2026-12de8",
+  storageBucket: "project-summer-2026-12de8.firebasestorage.app",
+  messagingSenderId: "88294145095",
+  appId: "1:88294145095:web:06736f99276fd1807c8562"
+};
+
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase接続成功");
